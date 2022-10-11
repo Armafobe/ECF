f9d26fea9a0ec004ce6833a80ed6838c703b4d33
Arthur <arthur_berthou@hotmail.com>
2022-10-11 15:16:28 +0200

    Create index.php

---
+++ dist/index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class index extends AbstractController
+{
+    #[Route('/index', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('home');
+    }
+}
