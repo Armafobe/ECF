015faa783da818873e7b86d7fc0168f119c80eb1
Arthur <arthur_berthou@hotmail.com>
2022-10-26 16:29:18 +0200

    Email type

--- src/Form/StructureRegistrationFormType.php
+++ src/Form/StructureRegistrationFormType.php
@@ -12,6 +12,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -25,7 +26,7 @@ class StructureRegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class)
             ->add('address', TextType::class)
             ->add('password', PasswordType::class, [
                 'mapped' => false,
